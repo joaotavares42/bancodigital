3104839f14b5c178d926fb1f408f527f3cdc241b
victor tavares <97231963+meik071@users.noreply.github.com>
2022-05-12 15:37:57 -0300

    banco digital

---
+++ app/Http/Requests/StoreTransferRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTransferRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'to_account' => 'exists:accounts,account_number',
+            'description' => 'required'
+            //'transfer' => 'gte:accounts,balance'
+        ];
+    }
+}
